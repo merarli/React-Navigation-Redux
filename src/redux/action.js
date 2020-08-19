caa32aee5797858b98889a5345b6737444b2d814
merarli <kenji@mera.tokyo>
2020-08-19 16:55:40 +0900

    redux ifx

--- src/redux/action.js
+++ src/redux/action.js
@@ -1,10 +1,3 @@
-export const setBadgeNumber = badgeNumber => (
-  {
-    type: 'SET_BADGE_NUMBER',
-    badgeNumber
-  }
-)
-
 export const setSearchText = searchText => (
   {
     type: 'SET_SEARCH_TEXT',
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,13 +1,9 @@
 const INITIAL_STATE = {
-  badgeNumber:0,
   searchText: 'でふぉると'
 }
 
 const reducer = (state=INITIAL_STATE, action) => {
   switch (action.type){
-    case "SET_BADGE_NUMBER":
-      return {...state, badgeNumber:action.badgeNumber}
-
     case "SET_SEARCH_TEXT":
       return {...state, searchText:action.searchText}
 
